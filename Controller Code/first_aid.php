50cb6cad0a599207a66f3d7c6d12758e65400f6b
Omsathivel T <113686229+omsakthivel123@users.noreply.github.com>
2024-07-30 14:09:53 +0530

    Add files via upload

---
+++ Controller Code/first_aid.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class first_aid extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->helper('file');
+        $this->load->model('first_aid_model');
+        $this->load->library('session');
+        $this->load->library('form_validation');
+    }
+
+
+    public function index()
+    {
+        $username = $this->session->userdata('userdata');
+        if (!empty($username) && isset($username['IsOnLogin']) && $username['IsOnLogin'] === TRUE) {
+
+
+            $this->load->view('Frontend/header');
+            $this->load->view('Frontend/sidebar');
+            $this->load->view('first_aid/first_aid');
+            $this->load->view('Frontend/footer');
+        } else {
+            redirect('Auth', 'refresh');
+        }
+    }}
